4a1b884636522f6572282be3cd4add8c32d4e455
Efeosa Eguavoen <33104551+foesa@users.noreply.github.com>
2019-11-24 21:06:10 +0000

    Cache implementation done!
    Completeed implementation of cache. Works for all values

--- CompArchCacher/cacher.py
+++ CompArchCacher/cacher.py
@@ -1,56 +1,117 @@
 import math
 
-hex2bin_map = {
-            "0": "0000",
-            "1": "0001",
-            "2": "0010",
-            "3": "0011",
-            "4": "0100",
-            "5": "0101",
-            "6": "0110",
-            "7": "0111",
-            "8": "1000",
-            "9": "1001",
-            "a": "1010",
-            "b": "1011",
-            "c": "1100",
-            "d": "1101",
-            "e": "1110",
-            "f": "1111",
-        }
 class cache():
     def __init__(self,L,N,K):
         self.L = L
         self.N = N
         self.K = K
-        self.cachetags = []
+        self.cachetags = {}
+        self.cacheLinked = {00:linkedList(),
+                            1:linkedList(),
+                            10:linkedList(),
+                            11:linkedList(),
+                            100:linkedList(),
+                            101:linkedList(),
+                            110:linkedList(),
+                            111:linkedList(),
+                            1000:linkedList()
+                            }
 
     def cacheCheck(self,Lister):
         if self.K == 1:
             for tag in Lister:
                 ctag = self.tagConvert(tag)
-                hitNum = 0
-                missnUm = 1
-                if ctag in self.cachetags:
-                    print(tag + "cache hit")
-                else:
-                    if (len(self.cachetags) < 8):
-                        print(tag + ctag + "cache miss")
-                        self.cachetags.append(ctag)
+                if ctag[1] in self.cachetags:
+                    if ctag[0] == self.cachetags[ctag[1]]:
+                        print(ctag,"hit")
                     else:
-                        for tager in self.cachetags:
-                            if tager[1] == ctag[1]:
-                                tager = ctag
+                        self.cachetags[ctag[1]] = ctag[0]
+                        print("miss")
+                else :
+                    self.cachetags[ctag[1]] = ctag[0]
+                    print("miss")
+        else:
+            hitNum = 0
+            for tag in Lister:
+                ctag = self.tagConvert(tag)
+                if int(ctag[1]) in self.cacheLinked:
+                    linkedList = self.cacheLinked[int(ctag[1])]
+                    if linkedList.swap(ctag[0],self.K) == True:
+                        print("hit",tag ,ctag)
+                        hitNum = hitNum+1
+                    else:
+                        print("miss",tag,ctag)
+            print(hitNum)
 
     def tagConvert(self,address):
-        fullTag = None
-        for i in address:
-            fullTag.append(hex2bin_map[i])
-        tagTuple = (fullTag[math.log(self.N,2)::-1],fullTag[4:math.log(self.N,2):-1])
+        binval = bin(int(address, 16))[2:]
+        binval = str(binval)
+        if len(binval) < 4+math.log(self.N,2):
+            while len(binval) <=  4+math.log(self.N,2):
+                binval = "0" + binval
+        tagTuple = (binval[0:len(binval)-(4+int(math.log(self.N,2)))],
+                    binval[len(binval)-(4+int(math.log(self.N,2))):len(binval)-4])
+        if tagTuple[0] == "" and tagTuple[1] == "":
+            return ('0','0')
+        elif tagTuple[0] == "" and tagTuple[1] != "":
+            return ('0',tagTuple[1])
+        elif tagTuple[0] != "" and tagTuple[1] == "":
+            return (tagTuple[0],'0')
         return tagTuple
+
+class linkedList():
+    def __init__(self):
+        self.head = None
+        self.length = 0
+    class node():
+        def __init__(self,val):
+            self.val = val
+            self.next = None
+
+    def swap(self,val,k):
+        if self.head  != None:
+            if self.head.val == val:
+                return True
+            else:
+                newTail = None
+                curNode = self.head
+                prevNode = None
+                while curNode != None and curNode.val != val:
+                    newTail = prevNode
+                    prevNode = curNode
+                    curNode = curNode.next
+                if(curNode != None):
+                    if (prevNode.val != self.head.val):
+                        prevNode.next = curNode.next
+                    else:
+                        self.head.next = curNode.next
+                    curNode.next = self.head
+                    self.head = curNode
+                    return True
+                else:
+                    if(self.length < k):
+                        noder = self.node(val)
+                        noder.next = self.head
+                        self.head =noder
+                        self.length = self.length +1
+                        return False
+                    else:
+                        newTail.next = None
+                        noder = self.node(val)
+                        noder.next = self.head
+                        self.head = noder
+                        return False
+        else:
+            noder = self.node(val)
+            self.head = noder
+            self.length += 1
+            return False
 def main():
-    cacher = cache(16,8,1)
+    cacher = cache(16,1,8)
     list = ["0000","0004","000c","2200","00d0","00e0","1130","0028","113c","2204","0010","0020","0004"
         ,"0040","2208","0008","00a0","0004","1104","0028","000c","0084","000c"
         ,"3390","00b0","1100","0028","0064","0070","00d0","0008","3394"]
-    cacher.tagConvert(cacher,list)
+    cacher.cacheCheck(list)
+
+if __name__ == '__main__':
+    main()
