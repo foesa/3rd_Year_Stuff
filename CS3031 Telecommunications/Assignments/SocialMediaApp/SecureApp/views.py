53ba5f6916a28338a29d5f6e2a0a3f9635b6419f
Efeosa Eguavoen <33104551+foesa@users.noreply.github.com>
2020-04-11 22:22:51 +0100

    finished site. both ppl must be friends for decryption to work. some other bug fixes here and there.

--- CS3031 Telecommunications/Assignments/SocialMediaApp/SecureApp/views.py
+++ CS3031 Telecommunications/Assignments/SocialMediaApp/SecureApp/views.py
@@ -1,8 +1,7 @@
-
+from cryptography.fernet import Fernet
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from accounts.models import userMake
 from .forms import HomeForm
 from .models import Post, userFriends
 from django.contrib.auth.decorators import login_required
@@ -10,11 +9,25 @@ class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get(self, request):
+        k = Fernet.generate_key()
+        crypter = Fernet(k)
         form = HomeForm()
         posts = Post.objects.all().order_by('-created')
         users = User.objects.exclude(id=request.user.id)
-        friend = userFriends.objects.get(current_user=request.user)
-        friends =  friend.users.all()
+        friend = userFriends.objects.filter(current_user=request.user)
+        friends = None
+        if(len(friend) > 0):
+            friends = userFriends.objects.get(current_user=request.user).users.all()
+        for i in posts:
+            otherUser = i.user
+            otherUserlist = userFriends.objects.filter(current_user=otherUser)
+            if(len(otherUserlist) > 0):
+                ousers = userFriends.objects.get(current_user=otherUser).users.all().filter(id=request.user.id).exists()
+            print(otherUserlist)
+            if(not i.user.id == request.user.id):
+                if friends is None or not friends.filter(id=i.user.id).exists() or not ousers:
+                    encrypted = crypter.encrypt(i.post.encode())
+                    i.post = encrypted
         args = {
             'form': form, 'posts': posts,'users': users, 'friends': friends
         }
@@ -41,3 +54,11 @@ def change_friends(request, operation, pk):
     elif operation == 'remove':
         userFriends.lose_friend(request.user, friend)
     return redirect('home')
+
+def change_friendsr(request, operation, pk):
+    friend = User.objects.get(pk=pk)
+    if operation == 'add':
+        userFriends.make_friend(request.user, friend)
+    elif operation == 'remove':
+        userFriends.lose_friend(request.user, friend)
+    return redirect('view_profile_with_pk')
--- CS3031 Telecommunications/Assignments/SocialMediaApp/accounts/views.py
+++ CS3031 Telecommunications/Assignments/SocialMediaApp/accounts/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import logout, login, authenticate
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from cryptography.fernet import Fernet
 from accounts.forms import (
     RegistrationForm,
 )
 from .forms import UserLoginForm
 from django.contrib.auth.models import User
+from SecureApp.models import Post, userFriends
+
 
 def login_view(request):
     next = request.GET.get('next')
@@ -36,11 +39,23 @@ def register(request):
 
 
 def view_profile(request, pk=None):
+    k = Fernet.generate_key()
+    crypter = Fernet(k)
     if pk:
         user = User.objects.get(pk=pk)
+        friend = userFriends.objects.get(current_user=user)
+        friends = friend.users.all()
+        isFriend = friends.filter(id=request.user.id).exists()
+        userPosts = Post.objects.filter(user_id=user.id)
+        if(not isFriend):
+            for i in userPosts:
+                encrypted = crypter.encrypt(i.post.encode())
+                i.post = encrypted
     else:
         user = request.user
-    args = {'user': user}
+        userPosts = Post.objects.filter(user_id=user.id)
+        isFriend = True
+    args = {'user': user,'posts':userPosts,"friend":isFriend}
     return render(request, 'profile.html', args)
 
 def logout_view(request):
